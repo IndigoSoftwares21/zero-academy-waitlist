f90fe562b8c668eb0df93d19ea4d5dcd1e7bd15b
Precious Orjiude <indigosoftwares21@gmail.com>
2023-01-06 08:56:42 +0200

    new

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m=5,n=7;
+    printf("I have one month of\n");
+    printf("Programming experience\n");
+    printf("m=%d\n",m);
+    printf("n=%d\n",n);
+    return 0;
+}
